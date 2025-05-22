fe5647d90f33c1ec82c40cf5ea1ebfe8539ba9f2
Rafi Sudrajat <rafisudrajat.99@gmail.com>
2025-05-22 13:35:37 +0700

    Add matplotlib library

--- app/main.cc
+++ app/main.cc
@@ -1,8 +1,15 @@
 #include "factorial.h"
 #include <iostream>
+#include "matplotlibcpp.h"
+
+namespace plt = matplotlibcpp;
 
 int main()
 {
+    plt::initialize_python();
     std::cout << factorial(5);
+
+    plt::plot({1, 2, 3, 4});
+    plt::show();
     return 0;
 }
