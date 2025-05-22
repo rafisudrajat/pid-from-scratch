b22760bb443315f757ac98df77a42a41e0a8bd3f
Rafi Sudrajat <rafisudrajat.99@gmail.com>
2025-05-22 13:54:47 +0700

    Add eigen library

--- app/main.cc
+++ app/main.cc
@@ -1,6 +1,8 @@
 #include "factorial.h"
 #include <iostream>
 #include "matplotlibcpp.h"
+#include "Eigen/Dense"
+using Eigen::MatrixXd;
 
 namespace plt = matplotlibcpp;
 
@@ -8,6 +10,13 @@ int main()
 {
     plt::initialize_python();
     std::cout << factorial(5);
+
+    MatrixXd m(2,2);
+    m(0,0) = 3;
+    m(1,0) = 2.5;
+    m(0,1) = -1;
+    m(1,1) = m(1,0) + m(0,1);
+    std::cout << m << std::endl;
 
     plt::plot({1, 2, 3, 4});
     plt::show();
